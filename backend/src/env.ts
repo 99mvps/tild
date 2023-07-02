0dcf271fd56d779acf457b82d1aeff6c320361f2
aldous huxley <root+github@99mvps.dev>
2023-07-02 11:42:15 -0300

    feat(alpha): another day, another humiliation

--- backend/src/env.ts
+++ backend/src/env.ts
@@ -1,13 +1,13 @@
 export function setEnvironment() {
   switch (process.env.NODE_ENV) {
     case "test":
-      return [".env.test", ".env"];
+      return [".env.test", "/etc/secrets/.env"];
     case "stage":
-      return [".env.stage", ".env"];
+      return [".env.stage", "/etc/secrets/.env"];
     case "development":
-      return [".env.development", ".env"];
+      return [".env.development", "/etc/secrets/.env"];
     case "production":
     default:
-      return ".env";
+      return "/etc/secrets/.env";
   }
 }
