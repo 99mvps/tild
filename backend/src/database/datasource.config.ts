4b314601a2d95cd554c5eb410d5606fe489785ac
aldous huxley <root+github@99mvps.dev>
2023-07-02 11:04:21 -0300

    feat(alpha): +1

--- backend/src/database/datasource.config.ts
+++ backend/src/database/datasource.config.ts
@@ -1,4 +1,6 @@
 import { DataSource } from "typeorm";
 import typeormConfig from "./typeorm.config";
 
+console.log("database configs", { typeormConfig });
+
 export const AppDataSource = new DataSource(typeormConfig);
